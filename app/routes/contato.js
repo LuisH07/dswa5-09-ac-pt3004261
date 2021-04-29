46e49a557c3c113b5fb04473a0f1d186de4648c8
Luis Malafaia <luishmalafaia@gmail.com>
2021-04-29 16:07:38 -0300

    final

--- app/routes/contato.js
+++ app/routes/contato.js
@@ -1,7 +1,7 @@
-modules.exports = function(app){
+module.exports = function(app){
     var controller = app.controllers.contato;
     app.route('/contatos')
-        .get(controller.listaConstatos);
+        .get(controller.listaContatos);
     app.route('/contatos/:id')
         .get(controller.obtemContato)
         .delete(controller.removeContato);
--- app/routes/curso.js
+++ app/routes/curso.js
@@ -1,4 +1,4 @@
-modules.exports = function(app){
+module.exports = function(app){
     var controller = app.controllers.curso;
     app.route('/cursos')
         .get(controller.listaCursos);
