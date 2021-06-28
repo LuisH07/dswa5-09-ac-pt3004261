dd9d4cb146768c566c2332dc0a9b9a4cb56ec0f6
Luis Malafaia <luishmalafaia@gmail.com>
2021-06-27 23:54:48 -0300

    primeiro commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ http.createServer(app).listen(app.get('port'), function(){
 });*/
 var http = require('http');
 var app = require('./config/express')();
-const url = "mongodb+srv://dswa5:dswa5@cluster0.fbpe2.mongodb.net/ifsp?retryWrites=true&w=majority";
+const url = 'mongodb+srv://dswa5:dswa5@cluster0.fbpe2.mongodb.net/ifsp?retryWrites=true&w=majority';
 require('./config/database.js')(url);
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express Server escutando na porta ' + app.get('port'));
